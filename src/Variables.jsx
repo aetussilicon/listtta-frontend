d0695ab4722829692742395bfdcd66a276f21dc0
Artur Gomes <arturgaollidev@outlook.com>
2024-03-30 16:12:53 -0300

    Criando variáveis.

---
+++ src/Variables.jsx
@@ -0,0 +1,17 @@
+export const variables = {
+
+    //Images
+    listttaLogo: 'Assets/icons/logo/colorful-logo.png',
+
+    //Icons
+    facebookMonoLogo: 'Assets/icons/social-networks/mono-facebook.svg',
+    instagramMonoLogo: 'Assets/icons/social-networks/mono-instagram.svg',
+
+    //Links
+    howWorksPage: '/how-it-works',
+    searchPage: '/search',
+    faqPage: '/faq',
+    aboutUsPage: '/about-us',
+    contactUsPage: '/contact'
+
+}
