975bfc0e230eae52f63f3b9fea3fe3a31df0a526
Artur Gomes <arturgaollidev@outlook.com>
2024-03-30 00:51:29 -0300

    Variáveis e header criados.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import "../../Styles/Components/Header.css"
+
+export default function Header() {
+
+    function menuClick() {
+        const menu = document.getElementById('menu-to-display');
+        if (menu.style.display == 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
+    }
+
+    return (
+        <header>
+            <nav>
+                <div className="container header-container">
+                    <div className="header-right-side">
+                        <div className="header-top-line">
+                            <a><img src="Assets/icons/logo/colorful-logo.png" alt="Listtta-logo" /></a>
+                            <span onClick={menuClick} className="material-symbols-outlined">menu</span>
+                        </div>
+                        <div id="menu-to-display" className="nav-links">
+                            <ul>
+                                <li>
+                                    <a>Home</a>
+                                </li>
+                                <li>
+                                    <a>Sobre o Listtta</a>
+                                </li>
+                                <li>
+                                    <a>Profissionais</a>
+                                </li>
+                                <li>
+                                    <a>Como funciona?</a>
+                                </li>
+                                <li>
+                                    <a>Contato</a>
+                                </li>
+                            </ul>
+                            <div id="menu-to-display" className="nav-register menu-buttons hidden-inverse">
+                                <button className="button">login</button>
+                                <a>Cadastre-se</a>
+                            </div>
+                        </div>
+                        <div className="nav-register hidden">
+                            <button><a>Login</a></button>
+                            <a>Cadastre-se</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
