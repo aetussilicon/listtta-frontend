c7e2ed5c782521947852fa61de0164e2da5537bb
Artur Oliveira <arturgaollidev@outlook.com>
2024-04-30 14:20:03 -0300

    Seção de escolha de tipo de usuário no login

--- src/Components/Signup/Login.jsx
+++ src/Components/Signup/Login.jsx
@@ -5,12 +5,25 @@ import '../../Styles/Components/LoginRegister/Login.css';
 
 export default function Login(props) {
     const [showLogin, setShowLogin] = useState('none');
+    const [showUserTypeSelection, setUserTypeSelection] = useState(false);
+
+    function menuClick() {
+        const menu = document.getElementById('login-signup-hidden');
+        const type = document.getElementById('login-type-hidden')
+        if (menu.style.display == 'block') {
+            menu.style.display = 'none';
+            type.style.display = 'block';
+        } else {
+            menu.style.display = 'block';
+            type.style.display = 'none';
+        }
+    }
 
     return (props.trigger) ? (
         <div className="login-container">
-            <div className="login-popup">
+            <div className="login-popup" id="login-signup-hidden">
                 <button className="login-close-button" onClick={() => props.setTrigger(false)}><img src={variables.closeMenu}></img></button>
-                <div className="login-signup-container">
+                <div className="login-signup-container" >
                     <div className="login-banner">
                         <img src={variables.loginBanner} alt="" />
                     </div>
@@ -21,7 +34,7 @@ export default function Login(props) {
                         </div>
                         <div className="login-buttons">
                             <button><img src="Assets/icons/social-networks/google.png" />Continuar com o Google</button>
-                            <a href={variables.signUpUser}><button><img src="Assets/icons/social-networks/email.png" />Continuar com o e-mail</button></a>
+                            <button onClick={menuClick}><img src="Assets/icons/social-networks/email.png" />Continuar com o e-mail</button>
                             <div className="login-breaker">
                                 <p>ou</p>
                             </div>
@@ -35,6 +48,36 @@ export default function Login(props) {
                                 Ao inscrever-se, você concorda com os <a href="#">Termos de Serviço</a> do Listtta
                                 e aceita receber nossos e-mails ocasionalmente. Leia nossa <a href="#">Política de Privacidade</a> para saber como usamos seus dados pessoais.
                             </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="login-popup" id="login-type-hidden">
+                <button className="login-close-button" onClick={() => props.setTrigger(false)}><img src={variables.closeMenu}></img></button>
+                <div className="login-signup-container user-chosser-container">
+                    <div className="who-you-are">
+                        <h1>Você é?</h1>
+                        <div className="who-you-are-block-one">
+                            <div className="who-block">
+                                <div className="who-block-content">
+                                    <img src="Assets/icons/accounts/tattoo.png" alt="Tattoo" />
+                                    <span><a href="/signup/tattoo">Tatuador</a></span>
+                                </div>
+                            </div>
+                            <div className="who-block">
+                                <div className="who-block-content">
+                                    <img src="Assets/icons/accounts/ear.png" alt="Piercer" />
+                                    <span><a href="/signup/piercer">Piercer</a></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="who-you-are-block-two">
+                            <div className="who-block block-two">
+                                <div className="who-block-content">
+                                    <img className="block-two-img" src="Assets/icons/accounts/customer.png" alt="Customer" />
+                                    <span><a href="/signup/user">Cliente</a></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
