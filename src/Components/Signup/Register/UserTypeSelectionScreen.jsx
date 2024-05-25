e6e507b44245e31e6ac9f4797941d68a2c275c88
Artur Oliveira <arturgaollidev@outlook.com>
2024-05-25 10:29:55 -0300

    Seleção de usuário atualizada
    Seleção de usuários atualizado para os botões atualizar o role do formulário de cadastro.

--- src/Components/Signup/Register/UserTypeSelectionScreen.jsx
+++ src/Components/Signup/Register/UserTypeSelectionScreen.jsx
@@ -1,8 +1,21 @@
 // import "../../../Styles/Components/Auth/Login.css";
+import { useContext, useEffect } from "react";
 import "../../../Styles/Components/Auth/SignupScreen.css";
 import "../../../Styles/Components/Auth/UserTypeSelectionScreen.css";
+import { SignupFormContext } from "../../../Contexts/SignupLoginFormContext";
 
 export default function UserTypeSelectionScreen(props) {
+  const { signupFormData, handleInputChange } =
+    useContext(SignupFormContext);
+
+  const handleRoleChange = (role) => {
+    handleInputChange(null, 'role', role);
+  }
+
+  useEffect(() => {
+    console.log(signupFormData);
+  }, [signupFormData]);
+
   return props.trigger ? (
     <div className="login-container">
       <div className="login-popup">
@@ -19,17 +32,17 @@ export default function UserTypeSelectionScreen(props) {
         <div className="signup-screen-container user-type-selection-screen-container">
           <div className="user-type-buttons-container">
             <div className="user-type-buttons-area-top">
-              <button className="user-type-button">
-                <img src="Assets/icons/accounts/tattoo.png" alt="Cliente" />
+              <button className="user-type-button"  onClick={() => handleRoleChange('TATTOO')} >
+                <img src="Assets/icons/accounts/tattoo.png" alt="Tatuador" />
                 <span className="default-span user-type-span">Tatuador</span>
               </button>
-              <button className="user-type-button">
-                <img src="Assets/icons/accounts/piercer.png" alt="Cliente" />
+              <button className="user-type-button" onClick={() => handleRoleChange('PIERCER')}>
+                <img src="Assets/icons/accounts/piercer.png" alt="Piercer" />
                 <span className="default-span user-type-span">Piercer</span>
               </button>
             </div>
             <div className="user-type-buttons-area-bottom">
-              <button className="user-type-button">
+              <button className="user-type-button" onClick={() => handleRoleChange('USER')} >
                 <img src="Assets/icons/accounts/customer.png" alt="Cliente" />
                 <span className="default-span user-type-span">Cliente</span>
               </button>
