599352252f14db6331b41b57a763f00fca370cbe
Artur Oliveira <arturgaollidev@outlook.com>
2024-07-09 20:10:02 +0000

    -> Placeholder da foto de usuário trocado. -> ProfessionalsCard atualizado par renderizar fotos de perfil

--- src/Components/FindProfessional/ProfessionalCards.jsx
+++ src/Components/FindProfessional/ProfessionalCards.jsx
@@ -13,25 +13,39 @@ export default function ProfessionalCards({ displayCount }) {
 
   return (
     <div className="professional-card-container">
-      {filteredProfessionals.map((professional) => (
-        <div key={professional.puid} className="professionals-card">
-          <div className="professional-picture">
-            {/* Imagem não utilizada */}
-          </div>
-          <div className="professional-info">
-            <h3>{professional.fullName}</h3>
-            <div className="professional-locale">
-              <img src="Assets/icons/miscellaneous/location.png" alt="" />
-              <p>{professional.address.city}/{professional.address.state}</p>
+      {filteredProfessionals.map((professional) => {
+        // Verifica se profilePicture é base64, se não, usa placeholderImage
+        const profilePicture = professional.profilePicture
+          ? `data:image/${professional.mimetype};base64,${professional.profilePicture}`
+          : "Assets/imgs/cards/profile-picture-placeholder.png";
+        console.log('Profile Picture:', profilePicture);
+
+        return (
+          <div key={professional.puid} className="professionals-card">
+            <div
+              className="professional-picture"
+              style={{
+                backgroundImage: `url(${profilePicture})`,
+              }}
+            ></div>
+            <div className="professional-info">
+              <h3>{professional.fullName}</h3>
+              <div className="professional-locale">
+                <img src="Assets/icons/miscellaneous/location.png" alt="" />
+                <p>{professional.address.city}/{professional.address.state} <br />
+                  {professional.address.cityZone !== null && (
+                    `Zona: ${professional.address.cityZone}`
+                  )}
+                </p>
+              </div>
+              <button className="btn">
+                <img src="Assets/icons/social-networks/mono-instagram.svg" alt="" />
+                @{professional.details.instagramUrl}
+              </button>
             </div>
-            <button className="btn">
-              <img src="Assets/icons/social-networks/mono-instagram.svg" alt="" />
-              @{professional.details.instagramUrl}
-            </button>
           </div>
-        </div>
-      ))}
-      {/* Adicione uma mensagem para indicar que não há resultados */}
+        );
+      })}
       {filteredProfessionals.length === 0 && <p>Nenhum profissional encontrado.</p>}
     </div>
   );
