5fd378888e7b8b391cef12e4f5c6c19ae190d414
Artur Oliveira <arturgaollidev@outlook.com>
2024-11-14 16:55:29 -0300

    Ajusta estilo e formatação na lista de profissionais

--- src/Pages/ProfessionalsList.jsx
+++ src/Pages/ProfessionalsList.jsx
@@ -1,61 +1,63 @@
-import Header from "../Components/Header/Header";
-import ProfessionalCards from "../Components/FindProfessional/ProfessionalCards";
-import "../Styles/Pages/ProfessionalsLists.css";
-import Footer from "../Components/Footer/Footer";
-import MainSearchFilter from "../Components/Filters/MainSearchFilter";
-import { useContext, useState } from "react";
-import { ProfessionalsContext } from "../Contexts/ProfessionalsContext";
-import MobileFilter from "../Components/FindProfessional/MobileFilter";
+import { useContext, useState } from 'react';
+import MainSearchFilter from '../Components/Filters/MainSearchFilter';
+import MobileFilter from '../Components/FindProfessional/MobileFilter';
+import ProfessionalCards from '../Components/FindProfessional/ProfessionalCards';
+import Footer from '../Components/Footer/Footer';
+import Header from '../Components/Header/Header';
+import { ProfessionalsContext } from '../Contexts/ProfessionalsContext';
+import '../Styles/Pages/ProfessionalsLists.css';
 
 export default function ProfessionalsLists() {
-  
-  const {resultsCount} = useContext(ProfessionalsContext);
+  const { resultsCount } = useContext(ProfessionalsContext);
   const [displayCount, setDisplayCount] = useState(12);
   const [filterMenu, setFilterMenu] = useState(false);
 
   const loadMoreCards = () => {
     setDisplayCount(displayCount + 12);
-  }
-  
+  };
+
   return (
     <>
       <Header />
-      <div className="search-banner">
-        <div className="container search-container">
+      <div className='search-banner'>
+        <div className='container search-container'>
           <h1>Resultados da sua Busca</h1>
           <span>
-            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-            consectetur, adipisci velit
+            Segue abaixo a lista dos artistas selecionados conforme a sua busca,
+            caso queira ver outros clique em Carregar Mais
           </span>
         </div>
       </div>
-      <div className="results-block">
-        <div className="results-area-container">
+      <div className='results-block'>
+        <div className='results-area-container'>
           <MainSearchFilter />
         </div>
-        <div className="professionals-results-area">
-          <div className="cards-results container results-container">
-            <span className="default-span">Total de resultados: {resultsCount} </span>
+        <div className='professionals-results-area'>
+          <div className='cards-results container results-container'>
+            <span className='default-span'>
+              Total de resultados: {resultsCount}{' '}
+            </span>
             <button
-              className="white-btn top-bar-professionals-button"
-              onClick={() => setFilterMenu(true)}
-            >
+              className='white-btn top-bar-professionals-button'
+              onClick={() => setFilterMenu(true)}>
               Filtros Avançados
             </button>
           </div>
-          <ProfessionalCards displayCount={displayCount}/>
-          <div className="load-more">
-              <button
-                className="btn load-more-btn"
-                onClick={loadMoreCards}
-              >
-                Carregar Mais
-              </button>
-            </div>
+          <ProfessionalCards displayCount={displayCount} />
+          <div className='load-more'>
+            <button
+              className='btn load-more-btn'
+              onClick={loadMoreCards}>
+              Carregar Mais
+            </button>
+          </div>
         </div>
       </div>
       <Footer />
-      <MobileFilter trigger={filterMenu} setTrigger={setFilterMenu} />
+      <MobileFilter
+        trigger={filterMenu}
+        setTrigger={setFilterMenu}
+      />
     </>
   );
 }
