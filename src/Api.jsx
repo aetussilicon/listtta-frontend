fd538e202f3fd1e8d3f3ddd151c52c9165a850f4
Artur Gomes <arturgaollidev@outlook.com>
2025-02-23 12:19:14 -0300

    fix: correção url

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  // baseURL: 'https://listtta-backend.lryftz.easypanel.host',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://listtta-backend.lryftz.easypanel.host',
+  // baseURL: 'http://localhost:8080',
 });
 
 export default Api;
